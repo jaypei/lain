fc66dfa5a7b6ec94dd6f6fa3ef17673a495943fd
jaypei <jaypei97159@gmail.om>
2014-12-26 23:30:59 +0800

    fix errors

--- util/init.lua
+++ util/init.lua
@@ -23,11 +23,32 @@ local screen       = screen
 local tonumber     = tonumber
 
 local wrequire     = require("lain.helpers").wrequire
+local first_line   = require("lain.helpers").first_line
 local setmetatable = setmetatable
 
 -- Lain utilities submodule
 -- lain.util
 local util = { _NAME = "lain.util" }
+
+-- Compatibility: Lua-5.1
+function split(str, pat)
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+     table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
+end
 
 -- Like awful.menu.clients, but only show clients of currently selected
 -- tags.
